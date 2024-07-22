cb53d7d19d13fd550c0d40dcef840d6240e55fba
Kim-soung-won <rlatmddnjs0103@naver.com>
2024-07-22 14:48:57 +0900

    JWT인증정보 추출 어노테이션

--- src/main/java/com/palette/palettepetsback/config/jwt/jwtAnnotation/LoginCheckMethodArgumentResolver.java
+++ src/main/java/com/palette/palettepetsback/config/jwt/jwtAnnotation/LoginCheckMethodArgumentResolver.java
@@ -15,13 +15,17 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 @Component
 @RequiredArgsConstructor
 public class LoginCheckMethodArgumentResolver implements HandlerMethodArgumentResolver {
+// HandlerMethodArgumentResolver : 컨트롤러 메서드의 매개변수 처리 방법을 정의한다.
+// HTTP 헤더, 세션, 쿼리 파라미터를 기반으로 매개변수를 설정할 수 있다.
 
     @Override
+    //JwtAuth 어노테이션이 존재하는지 확인한다.
     public boolean supportsParameter(MethodParameter parameter) {
         return parameter.hasParameterAnnotation(JwtAuth.class);
     }
 
     @Override
+    // JWT에서 인증정보를 가져오고 이를 AuthInfoDto로 변환하여 반환한다.
     public AuthInfoDto resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
         return JWTUtil.getMemberInfo();
     }
